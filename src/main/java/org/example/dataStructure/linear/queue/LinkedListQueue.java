645752b125ab2d18dad1f141416998d8b6fbd9a1
zs <2321136573@qq.com>
2025-11-16 22:13:41 +0800

    update LinkedListQueue

--- src/main/java/org/example/dataStructure/linear/queue/LinkedListQueue.java
+++ src/main/java/org/example/dataStructure/linear/queue/LinkedListQueue.java
@@ -1,66 +1,65 @@
 package org.example.dataStructure.linear.queue;
 
-import java.util.Collection;
 import java.util.Iterator;
-import java.util.Queue;
 
 public class LinkedListQueue<E>
         implements Queue<E>, Iterable<E> {
-    //节点
+
     private static class Node<E> {
         E value;
         Node<E> next;
 
-        public Node(E item, Node<E> next) {
-            this.value = item;
+        public Node() {
+        }
+
+        public Node(E value, Node<E> next) {
+            this.value = value;
             this.next = next;
         }
     }
 
-    Node<E> head = new Node<E>(null, null);
-    Node<E> tail = head;
-
+    Node<E> head=new Node<>(null,null);
+    Node<E> tail=head;
 
     public LinkedListQueue() {
-        tail.next = head;
+        tail.next=head;
     }
 
     @Override
-    public boolean add(E e) {
-        if(e == null) {
-            throw new NullPointerException();
-        }
-        Node<E> added=new Node<>(e,head);
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            Node<E> node=head.next;
+            @Override
+            public boolean hasNext() {
+
+
+                return node!=head;
+            }
+
+            @Override
+            public E next() {
+                E value=node.value;
+                node=node.next;
+                return value;
+            }
+        };
+    }
+
+    @Override
+    public boolean offer(E value) {
+        Node<E> added=new Node<>(value,head);
         tail.next=added;
         tail=added;
         return true;
-    }
-
-    @Override
-    public boolean offer(E e) {
-        Node<E> added=new Node<>(e, head);
-        tail.next=added;
-        tail=added;
-        return true;
-    }
-
-    @Override
-    public E remove() {
-        return null;
     }
 
     @Override
     public E poll() {
         if(isEmpty()) return null;
-        Node<E> removed=head.next;
-        head.next=removed.next;
-        if(removed==tail) tail=head;
-        return removed.value;
-    }
-
-    @Override
-    public E element() {
-        return null;
+        Node<E> node=head.next;
+        head.next=head.next.next;
+        if(node==tail) tail=head;
+        return node.value;
     }
 
     @Override
@@ -70,77 +69,8 @@ public class LinkedListQueue<E>
     }
 
     @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
     public boolean isEmpty() {
         return head==tail;
     }
-
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return new Iterator<E>() {
-            Node<E> current = head.next;
-            @Override
-            public boolean hasNext() {
-                return current!=head;
-            }
-
-            @Override
-            public E next() {
-                E value=current.value;
-                current=current.next;
-                return value;
-            }
-        };
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
 
 }
--- src/main/java/org/example/dataStructure/linear/queue/Queue.java
+++ src/main/java/org/example/dataStructure/linear/queue/Queue.java
@@ -1,4 +1,15 @@
 package org.example.dataStructure.linear.queue;
 
-public interface Queue {
+public interface Queue<E> {
+    /**
+     * 向队列尾部插入值
+     * @param e
+     * @return
+     */
+    boolean offer(E e);
+
+    E poll();
+    E peek();
+    boolean isEmpty();
+
 }
